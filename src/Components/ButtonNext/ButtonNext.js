a4cc44709a5e2743e177fa094286d331937d7876
Dmitry Radkovets <dmitry.radkovets@gmail.com>
2020-03-29 02:03:42 +0300

    feat: add ButtonNext component

---
+++ src/Components/ButtonNext/ButtonNext.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ButtonNext.css";
+
+export default function ButtonNext(props) {
+  return (
+    <button
+      className={
+        "button" + (props.active ? " button-active" : " button-passive")
+      }
+      onClick={props.onClick}
+    >
+      {props.name}
+    </button>
+  );
+}
